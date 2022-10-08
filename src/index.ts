9d263bb7cf8219854354e0e0ce41f79d7c1e128a
Pietro Piva Vieira <pietro.developer@gmail.com>
2022-10-08 19:11:16 -0300

    feat: Swagger docs

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import express from "express";
+import { serve, setup } from "swagger-ui-express";
 
 import { usersRoutes } from "./routes/users.routes";
+import swaggerFile from "./swagger.json";
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/api-docs", serve, setup(swaggerFile));
 
 app.use("/users", usersRoutes);
 
